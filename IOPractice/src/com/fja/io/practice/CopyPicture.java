9a697dda326741ecec62f54b37d8aeea6e42a500
asdmd177 <asdmd177@hotmail.com>
2018-08-12 11:49:34 +0800

    添加IO异常处理

--- IOPractice/src/com/fja/io/practice/CopyPicture.java
+++ IOPractice/src/com/fja/io/practice/CopyPicture.java
@@ -11,26 +11,40 @@ import java.io.IOException;
 public class CopyPicture {
 
 	public static void main(String[] args) {
-		try {
-			copy();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		copy();
 	}
 	
-	static void copy() throws IOException{
-		File origin = new File("E:\\java icon.jpg");
-		File dest = new File("D:\\java icon.jpg");
-		FileInputStream fis = new FileInputStream(origin);
-		FileOutputStream fos = new FileOutputStream(dest);
-		byte[] buffer = new byte[1024];
-		int len = 0;
-		while((len = fis.read(buffer))!=-1){					//边读边写
-			//因为buffer不会清空，因此每次读了多少数据就要写多少数据，fos.write(buffer)会导致重复复制倒数第二次的一部分数据。
-			fos.write(buffer,0,len);	
+	static void copy(){
+		FileInputStream fis = null;
+		FileOutputStream fos = null;
+		try{
+			File origin = new File("E:\\java icon.jpg");
+			File dest = new File("D:\\java icon.jpg");
+			fis = new FileInputStream(origin);
+			fos = new FileOutputStream(dest);
+			byte[] buffer = new byte[1024];
+			int len = 0;
+			while((len = fis.read(buffer))!=-1){					//边读边写
+				//因为buffer不会清空，因此每次读了多少数据就要写多少数据，fos.write(buffer)会导致重复复制倒数第二次的一部分数据。
+				fos.write(buffer,0,len);	
+			}
+		}catch(IOException e){
+			throw new RuntimeException(e);
+		}finally{
+			//【细节】释放资源的顺序，先开后关，后开先关。
+			try {
+				fos.close();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}finally{
+				//要让输入流和输出流释放资源的语句有机会执行，因此不能写在一个try/catch块中
+				try {
+					fis.close();
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			
 		}
-		//【细节】释放资源的顺序，先开后关，后开先关
-		fos.close();
-		fis.close();
 	}
 }
