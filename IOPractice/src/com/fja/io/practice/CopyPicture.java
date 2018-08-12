5fb2507cbaea0a298babf06faf8b993afc36c63f
asdmd177 <asdmd177@hotmail.com>
2018-08-12 22:25:52 +0800

    添加缓冲字节输入输出流拷贝图片练习

--- IOPractice/src/com/fja/io/practice/CopyPicture.java
+++ IOPractice/src/com/fja/io/practice/CopyPicture.java
@@ -1,5 +1,7 @@
 package com.fja.io.practice;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -11,9 +13,11 @@ import java.io.IOException;
 public class CopyPicture {
 
 	public static void main(String[] args) {
-		copy();
+		//copy();
+		bufferCopy();
 	}
 	
+	//使用文件字节流拷贝图片
 	static void copy(){
 		FileInputStream fis = null;
 		FileOutputStream fos = null;
@@ -45,6 +49,43 @@ public class CopyPicture {
 				}
 			}
 			
+		}
+	}
+	
+	
+	//使用缓冲字节流拷贝图片
+	static void bufferCopy(){
+		BufferedInputStream bis = null;
+		BufferedOutputStream bos = null;
+		try{
+			File origin = new File("E:\\java icon.jpg");
+			File dest = new File("D:\\java icon.jpg");
+			FileInputStream fis = new FileInputStream(origin);
+			FileOutputStream fos = new FileOutputStream(dest);
+			bis = new BufferedInputStream(fis);
+			bos = new BufferedOutputStream(fos);
+			int content = 0;
+			while(content!=-1){
+				content = bis.read();
+				if(content!=-1)bos.write(content);
+			}
+			bos.flush();//这里其实调不调用flush其实都一样，因为close()里面也会调用flush()
+		}catch(IOException e){
+			throw new RuntimeException(e);
+		}finally{
+			if(bos!=null){
+				try {
+					bos.close();
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}finally{
+					try {
+						bis.close();
+					} catch (IOException e) {
+						throw new RuntimeException(e);
+					}
+				}
+			}
 		}
 	}
 }
