0622798f9904a69226ce5dbff279d06282eaf3ed
asdmd177 <asdmd177@hotmail.com>
2018-09-01 18:35:32 +0800

    添加对象输入输出流

--- IOPractice/src/com/fja/io/object/ObjectInputstream.java
+++ IOPractice/src/com/fja/io/object/ObjectInputstream.java
@@ -9,18 +9,31 @@ import java.io.ObjectInputStream;
  * 把文件，内存中的信息读取出来就是反序列化 
  * 
  * 注意：
- * 对象输入流本身也是没有读写能力的
+ * 1. 对象输入流本身也是没有读写能力的
+ * 
+ * 2. 对象反序列化的时候会依赖本地的class文件，serialVersionUID是用于记录class文件信息，是这个ID
+ * 	      是通过一个类的类名、成员、包名、工程名计算出来的。
+ * 
+ * 3. 将对象序列化之后，又对类进行了修改，之后反序列化会出现异常，因为流中、文件中的对象的serialVersionUID
+ * 	      和本地的class文件的serialVersionUID冲突了，反序列化失败。修改类方法不会对serialVersionUID造成变化。
+ * 
+ * 4. 如果序列化与反序列化之间可能会修改类的成员，那么为了防止出现上述的serialVersionUID冲突异常，需要给类指定一个
+ *    serialVersionUID。有了serialVersionUID，jvm就不会给class文件计算serialVersionUID了。
  */
 public class ObjectInputstream {
 
 	public static void main(String[] args) {
-		readObject();		//姓名： 铁蛋 年龄： 1
+		readObject();		//姓名： 铁蛋  年龄： 1  薪水： 0.0  地址：中国
 	}
 	
 	public static void readObject(){
 		ObjectInputStream ois = null;
 		try {
 			ois = new ObjectInputStream(new FileInputStream("E:/obj.txt"));
+			/**
+			 * 问：对象的反序列化有对象产生，产生对象的时候会调用构造函数么？
+			 * 答：反序列化创造对象时，不会调用构造方法 
+			 */
 			Object obj = ois.readObject();
 			System.out.println((Person)obj);
 		}catch(ClassNotFoundException e){
--- IOPractice/src/com/fja/io/object/ObjectOutputstream.java
+++ IOPractice/src/com/fja/io/object/ObjectOutputstream.java
@@ -13,6 +13,8 @@ import java.io.Serializable;
  * 1. 对象输出流本身也是没有读写能力的
  * 2. 如果对象需要被写出到文件上，那么对象所属的类必须要实现Serializable接口
  * 3. 所有的集合都实现了Serializble接口
+ * 4. 类中那个属性不想被序列化，就用transient修饰。
+ * 5. 被序列化的对象中的成员变量如果是对象也需要实现serializable接口
  */
 public class ObjectOutputstream {
 
@@ -22,7 +24,8 @@ public class ObjectOutputstream {
 	
 	//将对象持久化到对象上
 	public static void writeObject(){
-		Person p = new Person("铁蛋",1);
+		Person p = new Person("铁蛋",1,5000.0,new Address("中国","北京"));
+		System.out.println(p);
 		ObjectOutputStream oos = null;
 		try{
 			FileOutputStream fos = new FileOutputStream("E:/obj.txt");
@@ -47,16 +50,41 @@ public class ObjectOutputstream {
  * Serializable接口中不包含任何的方法，是一个标志接口，标志着实现类的对象可以被序列化
  */
 class Person implements Serializable{
+	/**
+	 * 有了serialVersionUID，jvm就不会再给class文件计算serialVersionUID了。
+	 * 防止因为改变类而导致反序列化失败
+	 */
+	private static final long serialVersionUID = 1L;
+	
 	String name;
+	
 	int age;
-	public Person(String name, int age) {
+	
+	transient double salary;			//不想被序列化的成员变量
+	
+	Address address;
+	
+	public Person(String name, int age,double salary,Address address) {
 		super();
 		this.name = name;
 		this.age = age;
-	}
-	@Override
-	public String toString() {
-		return "姓名： "+this.name+" 年龄： "+this.age;
+		this.salary = salary;
+		this.address = address;
 	}
 	
+	@Override
+	public String toString() {
+		return "姓名： "+this.name+"  年龄： "+this.age+"  薪水： "+this.salary+"  地址："+this.address.country;
+	}
+}
+
+//被序列化的对象中的成员变量如果是对象也需要实现serializable接口
+class Address implements Serializable{
+	String country;
+	String city;
+	public Address(String country, String city) {
+		super();
+		this.country = country;
+		this.city = city;
+	}
 }
