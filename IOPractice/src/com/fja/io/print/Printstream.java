eae150d6db32c2f7295ab062560aeb90812251ba
e <asdmd177@hotmail.com>
2018-09-03 20:59:17 +0800

    添加使用打印流进行错误日志记录

--- IOPractice/src/com/fja/io/print/Printstream.java
+++ IOPractice/src/com/fja/io/print/Printstream.java
@@ -2,7 +2,12 @@ package com.fja.io.print;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.PrintStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.fja.io.FileOutputstream;
 
 /**
  * 打印流(PrintStream)：
@@ -34,7 +39,36 @@ public class Printstream {
 		//可以打印布尔值
 		ps.println(true);
 		//对象也可以打印
-		ps.println(new Cat("汤姆","grey",5));
+		ps.println(new Cat("汤姆","grey",5));//返回toString的结果
+		
+		//默认的输出就是向控制台输出
+		System.out.println("Hello World");
+		
+		//重新设置了输出流对象
+		System.setOut(ps);
+		//结果就输出到了E:/WriteTest.txt中去
+		System.out.println("Hello World");
+		
+		//打印流的另外一个用处就是收集异常日志信息
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		String date = sdf.format(new Date());
+		File errorLog = new File("E:/"+date+".log");
+		PrintStream logPrintStream = null;
+		try {
+			/**
+			 * 问：为什么要将FileOutputStream传入PrintStream的构造器中
+			 * 答：因为异常日志需要追加，不使用FileOutputStream无法追加内容
+			 */
+			logPrintStream = new PrintStream(new FileOutputStream(errorLog,true));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		try{
+			double res = 4/0;
+		}catch(Exception e){
+			//将写好的PrintStream传入printStackTrace()方法中，让异常输出到想要的目录中去
+			e.printStackTrace(logPrintStream);
+		}
 	}
 }
 
@@ -49,4 +83,10 @@ class Cat{
 		this.color = color;
 		this.age = age;
 	}
+	@Override
+	public String toString() {
+		return "名字： "+this.name+"  颜色： "+this.color+"  年龄： "+this.age;
+	}
+	
+	
 }
