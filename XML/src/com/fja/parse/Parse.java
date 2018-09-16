f64d784845f233e2fca06bcec9732acb4652e5d8
e <asdmd177@hotmail.com>
2018-09-16 22:32:14 +0800

    添加xmll文件注意事项

---
+++ XML/src/com/fja/parse/Parse.java
@@ -0,0 +1,18 @@
+package com.fja.parse;
+/**
+ * 解析的方式：
+ * DOM解析：
+ * 	1. JAXP	
+ * 	2. JDOM
+ * 	3. DOM4J (三大框架默认用DOM4J读取xml配置)
+ * 
+ * SAX解析
+ * 
+ */
+public class Parse {
+
+	public static void main(String[] args) {
+
+	}
+
+}
