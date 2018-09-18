11068f2ed0b5949c144297f8e260af116aba51f6
e <asdmd177@hotmail.com>
2018-09-18 22:41:08 +0800

    在控制台完整打印xml文件

---
+++ XML/src/com/fja/homework/PrintXML.java
@@ -0,0 +1,73 @@
+package com.fja.homework;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+
+import org.dom4j.Attribute;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.Node;
+import org.dom4j.Text;
+import org.dom4j.io 
+
+.SAXReader;
+import org.junit.Test;
+
+/**
+ *  需求：在控制台打印整个xml文档
+ */
+public class PrintXML {
+	@Test
+	public void print(){
+		SAXReader saxReader = new SAXReader();
+		
+		try {
+			Document dom = saxReader.read(new File("./src/com/fja/parse/xml/students.xml"));
+			Element rootElement = dom.getRootElement();
+			
+			String str = childNodes(rootElement);
+			
+			System.out.println(str);
+			
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String childNodes(Element elem){
+		Iterator<Node> nodeIterator = elem.nodeIterator();
+		//标签节点的缓冲字符序列
+		StringBuffer elemBuffer = new StringBuffer();
+		//属性节点序列
+		StringBuffer attrBuffer = new StringBuffer();
+		//标签名
+		String elemName = elem.getName();
+		
+		//遍历属性节点
+		List<Attribute> attributes = elem.attributes();
+		for(Attribute attr : attributes){
+			attrBuffer.append(" "+attr.getName()+"=\""+attr.getValue()+"\"");
+		}
+		String attrStr = attrBuffer.toString();
+		
+		elemBuffer.append("<"+elemName+attrStr+">");
+		
+		while(nodeIterator.hasNext()){
+			Node node = nodeIterator.next();
+			//文本节点
+			if(node instanceof Text) {
+				Text text = (Text)node;
+				elemBuffer.append(text.getText());
+			//标签节点
+			}else if(node instanceof Element) {
+				String childNodeStr = childNodes((Element)node);
+				elemBuffer.append(childNodeStr);
+			}
+		}
+		elemBuffer.append("</"+elemName+">");
+		
+		return elemBuffer.toString();
+	}
+}
---
+++ XML/src/com/fja/parse/Write.java
@@ -0,0 +1,97 @@
+package com.fja.parse;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
+import org.junit.Test;
+
+/**
+ * 对xml文档进行写入：
+ *  
+ */
+public class Write {
+	/**
+	 * 写入的基本步骤： 
+	 */
+	@Test
+	public void baseProcess(){
+		//1.创建一个写入对象。构造器可以接受文件字节输出流和文件字符输出流
+		XMLWriter xmlWriter = null;
+		try {
+			xmlWriter = new XMLWriter(new FileOutputStream("E:/XMLWrite.xml"));
+			//输出的内容，这里写出的是document对象
+			Document document = new SAXReader().read(new File("./src/com/fja/parse/xml/students.xml"));
+			//2.写出内容
+			xmlWriter.write(document);
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException(e);
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		} catch (DocumentException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}finally{
+			try {
+				//3.关闭输出流
+				if(xmlWriter!=null)xmlWriter.close();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+	
+	/**
+	 * 修改xml的内容 
+	 */
+	@Test
+	public void modify(){
+		XMLWriter xmlWriter = null;
+		try {
+			//1.读入document对象
+			Document document = new SAXReader().read(new File("./src/com/fja/parse/xml/students.xml"));
+			
+			//TODO 2.修改document对象
+			
+			
+			//3.将修改后的document对象写出到xml文档中
+			//3.1指定写出的格式
+			OutputFormat compact = OutputFormat.createCompactFormat();		//紧凑格式,没有空格换行,节约空间
+			OutputFormat pretty = OutputFormat.createPrettyPrint();			//漂亮格式,有空格换行,便于阅读
+			
+			//3.2指定生成的xml文档的编码,设置xml的编码和xml声明的encoding解码格式
+			//可以保证xml编码和解码的一致性，避免中文乱码的出现
+			compact.setEncoding("gbk");
+			
+			//3.3创建写出对象,XMLWriter的构造器可以接受两个参数，除了文件输出流之外，还可以接受一个格式对象（OutputFormat）。
+			xmlWriter = new XMLWriter(new FileOutputStream("E:/XMLWrite.xml"),compact);
+			
+			//3.4写出
+			xmlWriter.write(document);
+			
+		} catch (DocumentException e) {
+			throw new RuntimeException(e);
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException(e);
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}finally{
+			try {
+				//4.关闭输出流
+				if(xmlWriter!=null)xmlWriter.close();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+}
