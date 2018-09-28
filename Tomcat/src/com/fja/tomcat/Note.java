8402a5548566c0027b171fb4f1a4ff0a5bef9fec
e <asdmd177@hotmail.com>
2018-09-28 19:53:49 +0800

    添加手动运行servlet的步骤

--- Tomcat/src/com/fja/tomcat/Note.java
+++ Tomcat/src/com/fja/tomcat/Note.java
@@ -35,6 +35,39 @@ package com.fja.tomcat;
  * 					 找到服务器之后，通过端口找到监听这个端口的软件，进入默认对外共享文件的文件夹(tomcat中就是webapps),
  * 					 通过最后的资源名找到对应的资源。
  * 
+ * 
+ * 
+ * web项目的目录结构：
+ * 		|--WebRoot：
+ * 				|-- 静态资源(html、css、js、img、vedio)
+ * 				|-- WEB-INF
+ * 						|-- classes  存放class字节码文件(servlet)
+ * 						|-- lib      存放jar包文件
+ * 						|-- web.xml  配置文件
+ *	【注意】
+ *	WEB-INF目录下的资源不能通过浏览器直接访问
+ *	如果希望访问WEB-INF里的资源，需要将资源配置到web.xml中，例如servlet
+ *	
+ *
+ *	手动运行一个servlet的步骤
+ *		步骤一：
+ *			创建一个类，继承HttpServlet。重写当中的doGet、doPost方法
+ *		步骤二：
+ *			将步骤一中类的class字节码文件所包含的项目目录（注意是整个目录）拷贝到WEB-INF/classes/目录下
+ *		步骤三：
+ *			因为WEB-INF/目录下的资源不能直接被浏览器访问到，因此要在web.xml中进行servlet的配置
+ *			<servlet>
+ *				<servlet-name></servlet-name>
+ *				<servlet-class></servlet-class>
+ *			</servlet>
+ *
+ *			<servlet-mapping>
+ *				<servlet-name></servlet-name>
+ *				<url-pattern></url-pattern>
+ *			</servlet-mapping>
+ *		通过url-pattern找到servlet-name，再通过servlet-name找到servlet-class，最后通过servlet-class定位到class字节码文件
+ *		步骤四：
+ *			启动tomcat服务器，通过浏览器访问
  */
 public class Note {
 
